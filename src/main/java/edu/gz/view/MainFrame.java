f83e8304fe79a1de5fb77af787230f5a060c6f6c
Zach Goodson <quantamstudios@gmail.com>
2025-05-01 23:31:24 -0500

    loadPetData() -> loadPetAndExoticData()

--- src/main/java/edu/gz/view/MainFrame.java
+++ src/main/java/edu/gz/view/MainFrame.java
@@ -24,16 +24,23 @@ public class MainFrame extends JFrame {
 		petPanel = new PetPanel();
 		add(petPanel, BorderLayout.CENTER);
 
-		loadPetData();
+		loadPetAndExoticData();
 
 		setVisible(true);
 	}
 
-	private void loadPetData() {
+	private void loadPetAndExoticData() {
 		ShelterManager manager = JsonLoader.loadPets("/pets.json");
 
+		ShelterManager exoticManager = JsonLoader.loadExoticPets("/exotic_animals.json");
+
 		List<Pet> allPets = new ArrayList<>();
+
 		for (Map.Entry<String, Shelter<? extends Pet>> entry : manager.getAllShelters().entrySet()) {
+			allPets.addAll(entry.getValue().getAllPets());
+		}
+
+		for (Map.Entry<String, Shelter<? extends Pet>> entry : exoticManager.getAllShelters().entrySet()) {
 			allPets.addAll(entry.getValue().getAllPets());
 		}
 
