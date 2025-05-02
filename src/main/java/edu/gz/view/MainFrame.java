19338bcab05a9b9aa703f30bc184e8d869be883b
Zach Goodson <quantamstudios@gmail.com>
2025-05-01 22:25:30 -0500

    Update MainFrame to load pets.json

--- src/main/java/edu/gz/view/MainFrame.java
+++ src/main/java/edu/gz/view/MainFrame.java
@@ -1,26 +1,35 @@
 package edu.gz.view;
 
-import javax.swing.*;
-import java.awt.*;
+import edu.gz.utils.JsonLoader;
+import edu.gz.model.*;
+
+import javax.swing.JFrame;
+
+import java.awt.BorderLayout;
+import java.util.List;
 
 public class MainFrame extends JFrame {
 
-    private PetPanel petPanel;
+	private PetPanel petPanel;
 
-    public MainFrame() {
-        super("Adopt Me - Pet Adoption Center");
+	public MainFrame() {
+		super("Adopt Me - Pet Adoption Center");
 
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(800, 600);
-        setLocationRelativeTo(null);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(800, 600);
+		setLocationRelativeTo(null);
 
-        petPanel = new PetPanel();
-        add(petPanel, BorderLayout.CENTER);
+		petPanel = new PetPanel();
+		add(petPanel, BorderLayout.CENTER);
 
-        setVisible(true);
-    }
+		loadPetData();
 
-    public PetPanel getPetPanel() {
-        return petPanel;
-    }
+		setVisible(true);
+	}
+
+	private void loadPetData() {
+		List<Pet> petList = JsonLoader.loadPets("/pets.json");
+		PetTableModel model = new PetTableModel(petList);
+		petPanel.getPetTable().setModel(model);
+	}
 }
