eb27ca895506da16bca5591b12ee84c692cb7b5b
Zach Goodson <quantamstudios@gmail.com>
2025-05-01 20:15:13 -0500

    Add Dog, Cat, Rabbit Classes Back

--- src/main/java/edu/gz/model/ExoticAnimalAdapter.java
+++ src/main/java/edu/gz/model/ExoticAnimalAdapter.java
@@ -11,6 +11,5 @@ public class ExoticAnimalAdapter extends Pet {
 	
 	public ExoticAnimalAdapter(int id, String name, String type, String species, int age, boolean adopted) {
 		super(id, name, type, species, age, adopted);
-		
 	}
 }
---
+++ src/main/java/edu/gz/model/Rabbit.java
@@ -0,0 +1,7 @@
+package edu.gz.model;
+
+public class Rabbit extends Pet {
+	public Rabbit(int id, String name, String species, int age, boolean adopted) {
+		super(id, name, "Rabbit", species, age, adopted);
+	}
+}
