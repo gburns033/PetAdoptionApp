4930b9cce408532f99e42d4d2b1bc093bd79b57f
Zach Goodson <quantamstudios@gmail.com>
2025-05-01 20:58:11 -0500

    Add Main.java

---
+++ src/main/java/edu/gz/Main.java
@@ -0,0 +1,9 @@
+package edu.gz;
+
+import edu.gz.view.MainFrame;
+
+public class Main {
+    public static void main(String[] args) {
+        javax.swing.SwingUtilities.invokeLater(() -> new MainFrame());
+    }
+}
